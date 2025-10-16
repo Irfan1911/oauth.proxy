65a0c2d89ea1f806011e39051c1f30367c062580
Irfan1911 <irfan192106@gmail.com>
2025-10-16 21:06:16 +0800

    Scope Extension 0

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,7 +1,11 @@
 export default function handler(req, res) {
   const redirect_uri = 'https://oauth-proxy-steel.vercel.app/api/callback';
   const client_id = process.env.CLIENT_ID;
-  const scope = encodeURIComponent('https://www.googleapis.com/auth/drive.readonly');
+  const scope = [
+  "https://www.googleapis.com/auth/drive.readonly",
+  "https://www.googleapis.com/auth/spreadsheets"
+].join(" ");
+
   const state = encodeURIComponent('client_id=xyz'); // optional metadata
 
   const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&access_type=offline&prompt=consent&state=${state}`;
